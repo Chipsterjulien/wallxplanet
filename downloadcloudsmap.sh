f36b0083bfd693d91e6c47d049f4e3067804e308
Julien Freyermuth <julien.chipster@gmail.com>
2016-03-27 21:57:42 +0200

    adding somes line to get real clouds map and change a letter in conf file

--- downloadcloudsmap.sh
+++ downloadcloudsmap.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-curl -Ls -o /dev/null -w %{url_effective} http://xplanet.sourceforge.net/clouds/clouds_2048.jpg
-echo $url_effective
-
-#wget -N -c -O /tmp/clouds_2048.jpg http://eoimages.gsfc.nasa.gov/images/imagerecords/57000/57747/cloud_combined_2048.jpg
+cmd=`curl -LsI -o /dev/null -w "%{url_effective}\n" http://xplanet.sourceforge.net/clouds/clouds_2048.jpg | sed -e 's/.nyud.net:8080//'`
+wget -N -c -O /tmp/clouds_2048.jpg $cmd
