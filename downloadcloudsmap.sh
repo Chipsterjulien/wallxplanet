6d8cf9994d58986e471f04fb2e62d774f46222d8
Julien Freyermuth <julien.chipster@gmail.com>
2016-04-07 10:33:01 +0200

    Remove wget dependency and change output location to /media/tmpsfs

--- downloadcloudsmap.sh
+++ downloadcloudsmap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cmd=`curl -LsI -o /dev/null -w "%{url_effective}\n" http://xplanet.sourceforge.net/clouds/clouds_2048.jpg | sed -e 's/.nyud.net:8080//'`
-wget -N -c -O /tmp/clouds_2048.jpg $cmd
+curl -s -o /media/tmpfs/clouds_2048.jpg $cmd
--- wallxplanet.sh
+++ wallxplanet.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xplanet -conf /etc/wallxplanet/wallxplanet.conf -output /tmp/xplanet_earth.png -body earth -wait 120 -latitude 40 -longitude -4 -geometry 800x800
+xplanet -conf /etc/wallxplanet/wallxplanet.conf -output /media/tmpfs/xplanet_earth.png -body earth -wait 120 -latitude 40 -longitude -4 -geometry 800x800
